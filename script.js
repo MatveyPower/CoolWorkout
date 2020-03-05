9e014b08fb7c1c5f0646892a9e6bcab90ad5b8ee
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-03-05 14:20:48 +0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 'use strict'
-let correct = document.querySelector('.nameСorrectness input')
-console.log(correct)
+let nameCheck = document.querySelector('.nameCheck')
+let nameCheckButton = document.querySelector('.nameCheckButton')
+
+nameCheckButton.disabled = true;
+
+// nameCheck.addEventListener('')
+
+nameCheck.addEventListener('keydown', (event) => {
+    nameCheckButton.disabled = false
+    if (event.key === 'Backspace'){ 
+        nameCheckButton.disabled = true
+    }
+
+})
+
+
+function keyCheck(){
+    
+}
