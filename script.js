3082a946c28a86e3b2f5722b94d381310f9d8f4d
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-03-05 18:37:20 +0300

    button and input2

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict'
 let nameCheck = document.querySelector('.nameCheck')
 let nameCheckButton = document.querySelector('.nameCheckButton')
-
+let error = document.querySelector('.error')
+let questions = [...document.querySelectorAll('.question')]
+console.log(questions)
 nameCheckButton.disabled = true
 let count = 0;
 
@@ -11,12 +13,17 @@ nameCheck.addEventListener('keydown', keyCheck)
 nameCheckButton.addEventListener('click', () => {
     if (count < 3) {
         nameCheck.classList.add('nameCheckStyle')
+        error.style.display = 'block'
+    } else{
+        questions[0].style.display = 'block'
+
     }
 })
 
 
 function keyCheck(event) {
     nameCheck.classList.remove('nameCheckStyle')
+    error.style.display = 'none';
     if (event.key !== 'Backspace') {
         count++
     } else if (count > 0) {
