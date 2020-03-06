53f8a2155c8b5d708dd1c405c3d2b0ef684c8cce
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-03-06 17:11:26 +0300

    button and input

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 'use strict'
 let fieldForEnteringUserName = document.querySelector('.name-check')
-let userNameConfirmationButton = document.querySelector('.name-check-button')
-let error = document.querySelector('.error')
+let buttonToConfirmationUserName = document.querySelector('.name-check-button')
+let blockForDisplayingInputErrors = document.querySelector('.block-for-displaying-input-errors')
 let questions = [...document.querySelectorAll('.question')]
-let storeName = []
 
-userNameConfirmationButton.disabled = true
-let count = 0;
+
+buttonToConfirmationUserName.disabled = true
+
 
 fieldForEnteringUserName.addEventListener('input', keyCheck)
-userNameConfirmationButton.addEventListener('click', () => {
-    
+buttonToConfirmationUserName.addEventListener('click', () => {
+    if (fieldForEnteringUserName.value.length < 3) {
+        fieldForEnteringUserName.classList.add('displaying-input-errors')
+        blockForDisplayingInputErrors.style.display = 'block'
+    }
+
 })
 
 
 
-function keyCheck(event){
-    console.log(event)
-    
-        
-        console.log(fieldForEnteringUserName.value)
-    
+function keyCheck() {
+    blockForDisplayingInputErrors.style.display = 'none'
+    fieldForEnteringUserName.classList.remove('displaying-input-errors')
+    if (fieldForEnteringUserName.value.length > 0) {
+        buttonToConfirmationUserName.disabled = false
+    } else {
+        buttonToConfirmationUserName.disabled = true
+    }
 }
 
 
