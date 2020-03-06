359482c1e889367a91a19d69460cc1c6f8345152
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-03-06 18:53:25 +0300

    button and input

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let fieldForEnteringUserName = document.querySelector('.name-check')
 let buttonToConfirmationUserName = document.querySelector('.name-check-button')
 let blockForDisplayingInputErrors = document.querySelector('.block-for-displaying-input-errors')
 let questions = [...document.querySelectorAll('.question')]
-
+let listOfQuestions = document.querySelector('.questions')
 
 buttonToConfirmationUserName.disabled = true
 
@@ -15,8 +15,9 @@ buttonToConfirmationUserName.addEventListener('click', () => {
         fieldForEnteringUserName.classList.add('displaying-input-errors')
         blockForDisplayingInputErrors.style.display = 'block'
     } else{
-        blockUserName.textContent = `Здравствуйте, ${fieldForEnteringUserName.value}`
-
+        blockUserName.textContent = `Здравствуйте,${fieldForEnteringUserName.value}`
+        listOfQuestions.style.display = 'block'
+        questions[0].style.display = 'block'
     }
 
 })
