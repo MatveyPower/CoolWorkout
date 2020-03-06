0c1cbc99e027ca2d9249afda35fb869dc6c3dfb9
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-03-06 10:59:46 +0300

    button and input

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
 'use strict'
-let nameCheck = document.querySelector('.nameCheck')
-let nameCheckButton = document.querySelector('.nameCheckButton')
+let fieldForEnteringUserName = document.querySelector('.name-check')
+let userNameConfirmationButton = document.querySelector('.name-check-button')
 let error = document.querySelector('.error')
 let questions = [...document.querySelectorAll('.question')]
-console.log(questions)
-nameCheckButton.disabled = true
+let storeName = []
+
+userNameConfirmationButton.disabled = true
 let count = 0;
 
-// nameCheck.addEventListener('')
-
-nameCheck.addEventListener('keydown', keyCheck)
-nameCheckButton.addEventListener('click', () => {
-    if (count < 3) {
-        nameCheck.classList.add('nameCheckStyle')
-        error.style.display = 'block'
-    } else{
-        questions[0].style.display = 'block'
-
-    }
+fieldForEnteringUserName.addEventListener('input', keyCheck)
+userNameConfirmationButton.addEventListener('click', () => {
+    
 })
 
 
-function keyCheck(event) {
-    nameCheck.classList.remove('nameCheckStyle')
-    error.style.display = 'none';
-    if (event.key !== 'Backspace') {
-        count++
-    } else if (count > 0) {
-        count--
 
-    }
-
-    if (count > 0) {
-        nameCheckButton.disabled = false
-
-    } else {
-        nameCheckButton.disabled = true
-    }
+function keyCheck(event){
+    console.log(event)
+    
+        
+        console.log(fieldForEnteringUserName.value)
+    
 }
+
+
+
+
+
