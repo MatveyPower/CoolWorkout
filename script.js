40d2a81082621442624ad161b24d08a3cb65f34a
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-03-07 15:41:33 +0300

    questions start

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ let buttonToConfirmationUserName = document.querySelector('.name-check-button')
 let blockForDisplayingInputErrors = document.querySelector('.block-for-displaying-input-errors')
 let questions = [...document.querySelectorAll('.question')]
 let listOfQuestions = document.querySelector('.questions')
-
+let InputUserResponseToFirstQuestion = questions[0] 
+let buttonToConfirmationAnswer = document.querySelector('.button-answer')
 buttonToConfirmationUserName.disabled = true
+blockForDisplayingInputErrors.classList.add('display-none')
+listOfQuestions.classList.add('display-none')
+InputUserResponseToFirstQuestion.classList.add('display-none')
 
 
 fieldForEnteringUserName.addEventListener('input', keyCheck)
@@ -15,7 +19,7 @@ buttonToConfirmationUserName.addEventListener('click', buttonClick)
 
 
 function keyCheck() {
-    blockForDisplayingInputErrors.style.display = 'none'
+    blockForDisplayingInputErrors.classList.add('display-none')
     fieldForEnteringUserName.classList.remove('displaying-input-errors')
     if (fieldForEnteringUserName.value.length > 0) {
         buttonToConfirmationUserName.disabled = false
@@ -26,12 +30,13 @@ function keyCheck() {
 
 function buttonClick() {
     if (fieldForEnteringUserName.value.length < 3) {
+        blockForDisplayingInputErrors.classList.remove('display-none')
         fieldForEnteringUserName.classList.add('displaying-input-errors')
-        blockForDisplayingInputErrors.style.display = 'block'
     } else {
         blockUserName.textContent = `Здравствуйте,${fieldForEnteringUserName.value}`
-        listOfQuestions.style.display = 'block'
-        questions[0].style.display = 'block'
+        listOfQuestions.classList.remove('display-none')
+        InputUserResponseToFirstQuestion.classList.remove('display-none')
+        // questions[1].classList.remove('display-none')
     }
 }
 
